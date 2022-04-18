19e6f539e9a5c2663044238ebab8e078280c2523
David Shadoff <david.shadoff@gmail.com>
2022-04-18 19:43:47 -0400

    Fix CD head seek timing for PCE ( #4 )
    Fix timing.  This involced:
    - keeping a unified head position based on least read position
    - detecting data read (mode 6) and pce audio read transactions, and identifying positioning delay based on tables created through earlier research
    - delaying the status response from the CDROM unit for audio seek (until after seek was complete)
    - further delaying audio playback by roughly 250ms after CDROM status response
    Note: Delays were only implemented for PC Engine; other machines will require measurement and their own logic for implementation.

---
+++ mednafen/src/cdrom/seektime_pce.h
@@ -0,0 +1,12 @@
+/*
+ ============================================================================
+ Name        : seektime.h
+ Author      : Dave Shadoff
+ Version     :
+ Copyright   : (C) 2022 Dave Shadoff
+ Description : Program to determine seek time, based on start and end sector numbers
+ ============================================================================
+ */
+
+extern float get_pce_cd_seek_ms(int start_sector, int target_sector);
+
